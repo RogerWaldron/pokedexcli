fd686f4fef144f0073906fad1d1ad9e296d5ab5e
rwaldron <outsideinprofits@gmail.com>
2025-05-10 19:30:11 +1000

    Added test for CleanInput

---
+++ utils/input_test.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+    cases := []struct {
+		input string
+		expected string
+	}{
+		{
+			input: " hello world ",
+			expected: "hello world",
+		},
+		{
+			input: "helloworld ",
+			expected: "helloworld",
+		},
+		{
+			input: " hello-world ",
+			expected: "hello-world",
+		},
+	}
+
+	for _, c := range cases {
+		actual := CleanInput(c.input)
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("Expected: %v, Got: %v", expectedWord, word)
+			}
+		}
+	}
+}
