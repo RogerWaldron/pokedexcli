f4338f0ddc6dc387f75b87344baeaba4e7f5468c
rwaldron <outsideinprofits@gmail.com>
2025-05-12 14:28:50 +1000

    Fix: Added guard clause for empty struct

--- commands/command_catch.go
+++ commands/command_catch.go
@@ -4,20 +4,31 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"reflect"
+
+	"github.com/RogerWaldron/pokedexcli/internal/pokeapi"
 )
 
 func commandCatch(cfg *ApiConfig) error {
 	if len(cfg.Text) < 2 {
 		return nil
 	}
+
+	_, ok := cfg.Captured[cfg.Text[1]]
+	if ok {
+		return fmt.Errorf("you have already caught that pokemon")
+	}
+
 	pokemon, err := cfg.ApiClient.PokemonDetails(cfg.Text[1])
 	if err != nil {
 		return err
 	}
-	// TODO: FIX - above should error is pokemon doesn't exist
-	// Pokedex >catch picachu
-	// Throwing a Pokeball at s..
-    // panic: invalid argument to Intn
+
+	isEmpty := pokeapi.Pokemon{}
+
+	if reflect.DeepEqual(pokemon, isEmpty) {
+		return fmt.Errorf("%s does not exist", cfg.Text[1])
+	}
 
 	fmt.Printf("Throwing a Pokeball at %s...\n", pokemon.Name)
 	// The higher the base experience, the harder it should be to catch.
